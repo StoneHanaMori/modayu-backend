ea9b2dd0e128b33eaa38e735b59742af685d415f
sthAnd_hn <18376333@buaa.edu.cn>
2022-07-03 01:16:56 +0800

    article generate usecase

--- modayu/article/views.py
+++ modayu/article/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from yaml import serialize
 # Create your views here.
+
+@api_view(['POST'])
+def generate(request):
+    body = request.data.get("body", None)
+    if body is None:
+        return Response(data = {"detail" : "lack of body"}, status = status.HTTP_400_BAD_REQUEST)
+    title = "fake title"
+    abstract = "fake abstract"
+    serialize_data = {  "title" : title, 
+                        "body" : body,
+                        "abstract" : abstract}
+    return Response(data=serialize_data, status=status.HTTP_200_OK)
--- modayu/modayu/settings.py
+++ modayu/modayu/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'article',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,15 @@ WSGI_APPLICATION = 'modayu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'modayu',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True,
+        },
     }
 }
 
