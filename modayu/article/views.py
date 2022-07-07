06614efe1d1d869475679c06bc5f09fb0c4a5376
sthAnd_hn <18376333@buaa.edu.cn>
2022-07-07 22:58:51 +0800

    ok

--- modayu/article/views.py
+++ modayu/article/views.py
@@ -67,3 +67,21 @@ class ArticleViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        data = request.data.copy()
+        keywords = data.get("keywords", None)
+        if keywords is not None:
+            keywords = json.dumps(keywords)
+            serializer.save(keywords=keywords)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
